8a1f3b86343954006c779a9c4af07005ed36f8f0
MikeJohnP <tahoangphuc1901@gmail.com>
2023-10-24 22:23:31 +0700

    Can shoot bullets
    add spritesheet
    added bullets and function to shoot them

--- Mygame/main.cpp
+++ Mygame/main.cpp
@@ -1,36 +1,84 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <math.h>
 
+sf::Vector2f nomalizedVector(sf::Vector2f vector)
+{
+	float m = sqrt(vector.x * vector.x + vector.y * vector.y);
+	sf::Vector2f thenNomallized;
+	thenNomallized.x = vector.x / m;
+	thenNomallized.y = vector.y / m;
+	return thenNomallized;
+}
 int main()
 {
 	//----------------------INITIALIZE----------------------------
-	sf::RenderWindow window(sf::VideoMode(800, 800), "RPG Game",sf::Style::Default);
-	//----------------------INITIALIZE----------------------------
 	
-	//----------------------LOAD----------------------------
-	sf::Texture playTexture;
-	sf::Sprite playSprite;
-
-	if (playTexture.loadFromFile("Assets/Texture/spritesheet.png"))
+	sf::RenderWindow window(sf::VideoMode(1920, 1080), "RPG Game",sf::Style::Close);
+	//sf::RectangleShape bullets(sf::Vector2f(50, 25));
+	float bulletSpeed = 0.5f;
+	//----------------------INITIALIZE----------------------------
+	//-------------------------------LOAD----------------------------
+	//----------------------Skeleton----------------------------
+	
+	sf::Texture skeletonTexture;
+	sf::Sprite skeletonSprite;
+	if (skeletonTexture.loadFromFile("Assets/Player/Texture/spritesheet.png"))
 	{
-		playSprite.setTexture(playTexture);
+		skeletonSprite.setTexture(skeletonTexture);
+		int yIndex = 3;
+		int xIndex = 0;
+
+		skeletonSprite.setTextureRect(sf::IntRect(xIndex * 64, yIndex * 64, 64, 64));
+		skeletonSprite.setScale(sf::Vector2f(3, 3));
+		skeletonSprite.setPosition(sf::Vector2f(1650, 800));
+
+		std::cout << "Skeleton Texture is loaded" << std::endl;
+	}
+	else
+	{
+		std::cout << "Skeleton Texture is not loaded" << std::endl;
+	}
+
+	//----------------------Skeleton----------------------------
+	
+	//----------------------Player----------------------------
+	
+	sf::Texture playerTexture;
+	sf::Sprite playerSprite;
+
+	if (playerTexture.loadFromFile("Assets/Player/Texture/spritesheet.png"))
+	{
+		playerSprite.setTexture(playerTexture);
 
 		int yIndex = 0;
 		int xIndex = 0;
 
-		playSprite.setTextureRect(sf::IntRect(xIndex * 64, yIndex * 64,64,64));
-		playSprite.setScale(sf::Vector2f(3, 3));
+		playerSprite.setTextureRect(sf::IntRect(xIndex * 64, yIndex * 64,64,64));
+		playerSprite.setScale(sf::Vector2f(3, 3));
 		
-		std::cout << "Texture is loaded" << std::endl;
+		std::cout << "Player Texture is loaded" << std::endl;
 	}
 	else
 	{
-		std::cout << "Texture is not loaded" << std::endl;
+		std::cout << "Player Texture is not loaded" << std::endl;
 	}
-	//----------------------LOAD----------------------------
+
+	//----------------------Player----------------------------
+	
+	//----------------------Bullet----------------------------
+	//bullets.setPosition(playerSprite.getPosition());
+	//sf::Vector2f directions = skeletonSprite.getPosition() - bullets.getPosition();
+	//directions = nomalizedVector(directions);
+	//----------------------Bullet----------------------------
+	//-------------------------------LOAD----------------------------
+	
 	//----------------------UPDATE----------------------------
+	std::vector<sf::RectangleShape> bullets;
+	sf::Vector2f direction;
 	while (window.isOpen())
 	{
+		
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
@@ -38,25 +86,53 @@ int main()
 				window.close();
 			
 		}
+
 	//----------------------UPDATE----------------------------
+	
+		//bullets.setPosition(bullets.getPosition() + directions * bulletSpeed);
+	//----------------------PlayerMovement----------------------------
 
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
-			playSprite.move(sf::Vector2f(0.1, 0.f));
+			playerSprite.move(sf::Vector2f(1, 0.f));
 
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
-			playSprite.move(sf::Vector2f(0.f, 0.1));
+			playerSprite.move(sf::Vector2f(0.f, 1));
 		
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
-			playSprite.move(sf::Vector2f(0.f, -0.1));
+			playerSprite.move(sf::Vector2f(0.f, -1));
 
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
-			playSprite.move(sf::Vector2f(-0.1, 0.f));
-		
-	//----------------------DRAW----------------------------
+			playerSprite.move(sf::Vector2f(-1, 0.f));
+
+		if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+		{
+			bullets.push_back(sf::RectangleShape(sf::Vector2f(50, 25)));
+			int i = bullets.size() - 1;
+			bullets[i].setPosition(playerSprite.getPosition());
+		}
+	
+	//----------------------PlayerMovement----------------------------
+		for (size_t i = 0; i < bullets.size(); i++)
+		{
+			sf::Vector2f direction = skeletonSprite.getPosition() - bullets[i].getPosition();
+			direction = nomalizedVector(direction);
+			bullets[i].setPosition(bullets[i].getPosition() + direction * bulletSpeed);
+
+	//--------------------DRAW----------------------------
+		}
 		window.clear(sf::Color::Black);
-		window.draw(playSprite);
+		window.draw(skeletonSprite);
+		window.draw(playerSprite);
+		
+		for (size_t i = 0; i < bullets.size(); i++)
+		{
+			window.draw(bullets[i]);
+		}
+		
 		window.display();
+
 	//----------------------DRAW----------------------------
 	}
 	return 0;
+	
 }
